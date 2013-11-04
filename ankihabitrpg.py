a7305174965069f31782073950f48a63c7933a80
Pherr <pherr@pherr.net>
2013-11-04 14:03:32 -0700

    Rename variables

--- ankihabitrpg.py
+++ ankihabitrpg.py
@@ -8,26 +8,32 @@ from anki.sync import Syncer
 from aqt import *
 
 config ={}
-rate = 5
-correct_answers = 0
-active = False
-url = 'https://habitrpg.com/api/v1/user/task/Anki/up'
+Syncer.rate = 5
+Syncer.correct_answers = 0
+Syncer.active = False
+url = 'https://beta.habitrpg.com/api/v1/user/task/Anki/up'
+headers = {}
 
 configpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "habitrpg.config")
 configpath = configpath.decode(sys.getfilesystemencoding())
 
 def card_answered(self, ease):  # Cache number of correct answers
-    if active == True:
+    if Syncer.active == True:
         if ease > 1:
-            correct_answers += 1
+            Syncer.correct_answers += 1
 
 def habit_sync(x):              # Call API once for every correct answer during Ankiweb sync
-    if active == True:
-        while correct_answers >= rate:
-            urllib2.urlopen(url,urllib.urlencode(Syncer.headers))
-            correct_answers -= rate
-        config['score'] = correct_answers
+    if Syncer.active == True:
+        utils.showInfo("Enter sync function")
+        request = urllib2.Request(url, headers=headers)
+        while Syncer.correct_answers >= Syncer.rate:
+            utils.showInfo("Enter loop")
+            urllib2.urlopen(url,urllib.urlencode(headers))
+        utils.showInfo("Exit loop")
+        config['score'] = Syncer.correct_answers
+        utils.showInfo("Save score")
         json.dump( config, open( configpath, 'w' ) )
+        utils.showInfo("Dump JSON and exit")
         
 
 def setup():
@@ -37,10 +43,10 @@ def setup():
         if ok == True:          # Create config file and save values
             api_token = str(api_token)
             user_id = str(user_id)
-            config = {'token' : api_token, 'user' : user_id, 'score' : correct_answers}
+            config = {'token' : api_token, 'user' : user_id, 'score' : Syncer.correct_answers}
             json.dump( config, open( configpath, 'w' ) )
-            active = True
-            Syncer.headers = {"x-api-key":api_token, "x-api-user":user_id}
+            Syncer.active = True
+            headers = {"x-api-user":user_id, "x-api-key":api_token, "Content-Type":"application/json"}
             utils.showInfo("The add-on has been setup.")
 
 
@@ -48,9 +54,9 @@ if os.path.exists(configpath):    # Load config file
     config = json.load(open(configpath, 'r'))
     api_token = config['token']
     user_id = config['user']
-    correct_answers = config['score']
-    Syncer.headers = {"x-api-key":api_token, "x-api-user":user_id}
-    active = True
+    Syncer.correct_answers = config['score']
+    headers = {"x-api-user":user_id, "x-api-key":api_token, "Content-Type":"application/json"}
+    Syncer.active = True
 
 
 #Add Setup to menubar
@@ -60,4 +66,4 @@ mw.form.menuTools.addAction(action)
 
 #Wrap funtions to Anki
 Reviewer._answerCard = wrap(Reviewer._answerCard, card_answered)
-Syncer.sync = wrap(Syncer.sync, habit_sync)
+Syncer.sync = wrap(Syncer.sync, habit_sync, "before")
